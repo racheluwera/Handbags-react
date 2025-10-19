2892eaeb9440a66f8b28c35366c44965ddf7c1cd
racheluwera <racheluwera9@gmail.com>
2025-10-19 17:30:15 +0200

    Added About us contents

--- src/about.jsx
+++ src/about.jsx
@@ -1,5 +1,20 @@
 function About() {
-  return <h1>About Page</h1>;
+  return (
+    <>
+    <div className="text-center my-8">
+    <h1 className="text-2xl"><b>About us</b></h1>
+    <p>At HandBagStore, we believe that a handbag is more than just an accessory — it’s a statement of confidence, elegance, and individuality.
+Our journey began with a passion for timeless fashion and a commitment to quality craftsmanship.
+We curate a diverse collection of handbags designed for every occasion — from everyday essentials to luxury pieces that define your personal style.
+Each bag is carefully made with premium materials to ensure durability, comfort, and lasting beauty</p>
+<br/><br/>
+ <h1 className="text-2xl"><b>Our Mission</b></h1>
+ <p>Our mission is to empower every woman to express her unique style through premium, affordable handbags that combine fashion with function.
+We strive to deliver excellence in both design and service, ensuring that every customer feels valued, inspired, and confident with every purchase.
+At HandBagStore, your satisfaction is our top priority — because you deserve to carry more than just a bag; you deserve to carry confidence</p>
+</div>
+</>
+  );
 }
 
 export default About;
